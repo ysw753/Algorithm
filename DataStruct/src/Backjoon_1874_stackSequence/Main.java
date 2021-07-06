05b44a7b66b1a5f9cfb69b562859c1d5c7abee63
ysw753 <81672833+ysw753@users.noreply.github.com>
2021-07-06 19:40:55 +0900

    Backjoon_1874_stackSequence
    출력초과 되지 않으려면 StringBuilder써야함

---
+++ DataStruct/src/Backjoon_1874_stackSequence/Main.java
@@ -0,0 +1,39 @@
+package Backjoon_1874_stackSequence;
+
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+		Stack<Integer> stack = new Stack<>();
+		StringBuilder sb= new StringBuilder();
+		int num = scan.nextInt();
+		
+		
+		int point=0;
+		while(num-->0)
+		{
+			int input = scan.nextInt();
+		
+			if(input>point) {
+				for(int i=point+1;i<=input;i++){		
+					stack.push(i);
+					sb.append('+').append('\n');
+				}
+				point=input;
+			}
+			else if(stack.peek()!=input) {
+				System.out.println("NO");
+				return;
+			}
+			stack.pop();
+			sb.append('-').append('\n');
+		}
+		System.out.println(sb);
+	}
+
+}
